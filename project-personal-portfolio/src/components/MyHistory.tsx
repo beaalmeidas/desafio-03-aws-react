4e6edbdc85ea55ad81c8eab317a401bdcb2890fe
beaalmeidas <beatrizalmeidabss@gmail.com>
2024-11-12 11:03:44 -0300

    style: melhorando estilização da caixa de texto de Minha História

--- project-personal-portfolio/src/components/MyHistory.tsx
+++ project-personal-portfolio/src/components/MyHistory.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useUser } from '../context/useUser';
 
 // CSS
 import '../styles/component-styles/MyHistoryStyle.css';
 
-
 const MyHistory: React.FC = () => {
     const { user, setUser } = useUser();
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    // AJUSTAR ALTURA DA CAIXA DE TEXTO
+    useEffect(() => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = 'auto';
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        }
+    }, [user!.userHistory]);
+
+    const handleInputChange = (value: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setUser({ ...user!, userHistory: value.target.value });
+    };
 
     return (
-
         <div id='my-history-content'>
-            {/* TÍTULO */}
             <p id='my-history-title'>Minha história</p>
 
-            {/* CAIXA DE TEXTO PARA ADICIONAR HISTÓRIA */}
-            <input
+            {/* CAIXA DE TEXTO PARA HISTÓRIA */}
+            <textarea
+                ref={textAreaRef}
                 id='my-history-input'
                 readOnly={!user!.isEditing}
-                type="text"
                 value={user!.userHistory}
-                onChange={(value)=>setUser({...user!,userHistory:value.target.value})}
+                onChange={handleInputChange}
+                onInput={() => {
+                    if (textAreaRef.current) {
+                        textAreaRef.current.style.height = 'auto';
+                        textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+                    }
+                }}
             />
         </div>
     );
 };
-
 
 export default MyHistory;
